3c5d8ac637895b844bd35d0c6ee001267187e205
Aimer <1428616906@qq.com>
2018-11-26 20:36:24 +0800

    修改配置文件

--- bill/src/main/java/edu/zut/software/bill/controller/UserController.java
+++ bill/src/main/java/edu/zut/software/bill/controller/UserController.java
@@ -1,4 +1,23 @@
 package edu.zut.software.bill.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
 public class UserController {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @ResponseBody
+    @GetMapping("/jdbc")
+    public Map<String, Object> map() {
+        List<Map<String, Object>> list = jdbcTemplate.queryForList("select * from manager");
+        return list.get(0);
+    }
 }
